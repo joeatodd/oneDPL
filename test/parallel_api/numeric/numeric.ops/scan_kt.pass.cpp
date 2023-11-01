8fdd4ef67bdfbabe6739cf0199cfb11e449c9158
Alberto Cabrera <alberto.cabrera@codeplay.com>
2023-11-01 22:54:35 +0000

    Avoid race conditions in the test

--- test/parallel_api/numeric/numeric.ops/scan_kt.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/scan_kt.pass.cpp
@@ -22,23 +22,23 @@ int
 main()
 {
     bool all_passed = true;
+    sycl::queue q;
 
     for (int logn : {4, 8, 11, 16, 19, 21})
     {
         std::cout << "Testing 2^" << logn << '\n';
         int n = 1 << logn;
         std::vector<int> v(n, 1);
-        sycl::queue q;
         int* in_ptr = sycl::malloc_device<int>(n, q);
         int* out_ptr = sycl::malloc_device<int>(n, q);
 
-
-        q.copy(v.data(), in_ptr, n);
+        q.copy(v.data(), in_ptr, n).wait();
         using KernelParams = oneapi::dpl::experimental::kt::kernel_param<128, 2, class ScanKernel>;
         oneapi::dpl::experimental::kt::single_pass_inclusive_scan<KernelParams>(q, in_ptr, in_ptr+n, out_ptr, ::std::plus<int>());
 
         std::vector<int> tmp(n, 0);
         q.copy(out_ptr, tmp.data(), n);
+        q.wait();
 
         std::inclusive_scan(v.begin(), v.end(), v.begin());
 
@@ -48,7 +48,7 @@ main()
             if (tmp[i] != v[i])
             {
                 passed = false;
-                std::cout << "expected " << i << ' ' << v[i] << ' ' << tmp[i] << '\n';
+                // std::cout << "expected " << i << ' ' << v[i] << ' ' << tmp[i] << '\n';
             }
         }
 
@@ -58,6 +58,8 @@ main()
             std::cout << "failed" << std::endl;
 
         all_passed &= passed;
+        sycl::free(in_ptr, q);
+        sycl::free(out_ptr, q);
     }
 
     return !all_passed;
